d61401e0eb10e1adbe867a53b16f4e21234c6aa8
John Phillips <madhatter003@yahoo.com>
2019-04-18 14:32:04 -0600

    added conditionals to update login status dependant on if user's session has ended or not

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -41,7 +41,7 @@ export default class PortfolioContainer extends Component {
 
 	portfolioItems() {
 		return this.state.data.map((item) => {
-			return <PortfolioItem key={item.id} item={item}  />;
+			return <PortfolioItem key={item.id} item={item} />;
 		});
 	}
 
@@ -55,13 +55,10 @@ export default class PortfolioContainer extends Component {
 		}
 
 		return (
-			<div>
-				<h2>{this.state.pageTitle}</h2>
-
-				<button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-				<button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-				<button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
+			<div className="portfolio-items-wrapper">
+				<button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+				<button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+				<button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
 				{this.portfolioItems()}
 			</div>
 		);
